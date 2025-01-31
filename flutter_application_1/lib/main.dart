27e75aa0e2da9b7050f6808183659027ab724370
Leozera2k <leofsousa98@gmail.com>
2025-01-31 09:55:28 -0300

    Organizando  aplicação

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/home_page.dart';
+import 'my_app.dart';
 
 
 void main(){
   runApp( const MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-      );
-  }
-}
-
-
-
