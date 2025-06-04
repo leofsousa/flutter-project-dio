fe34b989ee3563221c0b6a75318a7c3554ceef65
Leozera2k <leofsousa98@gmail.com>
2025-06-04 15:42:36 -0300

    Fazendo o App

--- flutter_apk_1/lib/main.dart
+++ flutter_apk_1/lib/main.dart
@@ -1,46 +1,9 @@
+
 import 'package:flutter/material.dart';
+
+import 'my_app.dart';
+
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        appBarTheme: const AppBarTheme(
-          color: Colors.red
-        )
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My App'),
-        ),
-      body: const Center(child: Text('Olá')),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add_box),
-        onPressed: () {}
-        ),
-    );
-  }
 }
