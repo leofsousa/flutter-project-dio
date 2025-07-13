54bd832aa6aecce41d67bd157e6a5e01198267f7
Leozera2k <leofsousa98@gmail.com>
2025-07-13 14:49:17 -0300

    new page dados cadastrais

--- flutter_apk_1/lib/pages/main_page.dart
+++ flutter_apk_1/lib/pages/main_page.dart
@@ -10,11 +10,51 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Meu App"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Main Page"),
+        ),
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Dados Cadastrais")),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Termos de Uso e Privacidade")),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    width: double.infinity,
+                    child: const Text("Configurações")),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      drawer: const Drawer(),
     );
   }
 }
