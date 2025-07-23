4f5eec9e2acd0ac1329cff22f2cbc821ff263db1
Leozera2k <leofsousa98@gmail.com>
2025-07-23 15:13:30 -0300

    separando o drawer

--- flutter_apk_1/lib/pages/main_page.dart
+++ flutter_apk_1/lib/pages/main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_apk_1/pages/dados_cadastrais.dart';
 import 'package:flutter_apk_1/pages/pagina1.dart';
 import 'package:flutter_apk_1/pages/pagina2.dart';
 import 'package:flutter_apk_1/pages/pagina3.dart';
+import 'package:flutter_apk_1/shared/widgets/custom_drawer.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -21,52 +22,7 @@ class _MainPageState extends State<MainPage> {
         appBar: AppBar(
           title: const Text("Main Page"),
         ),
-        drawer: Drawer(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InkWell(
-                  child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      width: double.infinity,
-                      child: const Text("Dados Cadastrais")),
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const DadosCadastraisPage(
-                                )));
-                  },
-                ),
-                const Divider(),
-                const SizedBox(
-                  height: 20,
-                ),
-                InkWell(
-                  child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      width: double.infinity,
-                      child: const Text("Termos de Uso e Privacidade")),
-                  onTap: () {},
-                ),
-                const Divider(),
-                const SizedBox(
-                  height: 20,
-                ),
-                InkWell(
-                  child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      width: double.infinity,
-                      child: const Text("Configurações")),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
+        drawer: const CustomDrawer(),
         body: Column(
           children: [
             Expanded(
---
+++ flutter_apk_1/lib/shared/widgets/custom_drawer.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_apk_1/pages/dados_cadastrais.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: Text("Câmera"),
+                          leading: Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          leading: Icon(Icons.album),
+                          title: Text("Galeria"),
+                        )
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+                currentAccountPicture: CircleAvatar(
+                  child: Image.network(
+                      "https://c5gwmsmjx1.execute-api.us-east-1.amazonaws.com/prod/dados_processo_seletivo/logo_empresa/119818/LOGO-DIO-COLOR.png_name_20221031-2831-ekn5hh.png"),
+                ),
+                accountName: Text("Leonardo Sousa"),
+                accountEmail: Text("email@email.com")),
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Dados Cadastrais"),
+                  ],
+                )),
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DadosCadastraisPage()));
+            },
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    Icon(Icons.info),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Termos de Uso e Privacidade"),
+                  ],
+                )),
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 12),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                "Termos de Uso e Privacidade",
+                                style: TextStyle(
+                                    fontSize: 24, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Text(
+                              "Levando em consideração as tendências globais, o surgimento do comércio virtual talvez venha a ressaltar a relatividade da governabilidade em ambientes voláteis. O incentivo ao avanço tecnológico, assim como a estrutura atual da organização demanda um esforço conjunto de alinhamento das diretrizes de desenvolvimento para o futuro. Percebemos, cada vez mais, que a competitividade nas transações comerciais ilustra as tensões entre tradição e inovação das posturas dos órgãos dirigentes com relação às suas atribuições. No escopo da atual conjuntura, a contínua expansão de nossa atividade garante a contribuição de um grupo importante na determinação dos processos que moldam a realidade institucional.",
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            },
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                width: double.infinity,
+                child: const Row(
+                  children: [
+                    Icon(Icons.settings),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Configurações"),
+                  ],
+                )),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
