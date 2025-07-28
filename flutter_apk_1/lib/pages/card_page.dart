968bf9797fe9e107c3380605e64fb9ff3ca307b6
Leozera2k <leofsousa98@gmail.com>
2025-07-28 11:30:41 -0300

    Hero e LinearProgressIndicator

--- flutter_apk_1/lib/pages/card_page.dart
+++ flutter_apk_1/lib/pages/card_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apk_1/model/card_detail.dart';
+import 'package:flutter_apk_1/pages/card_detail.dart';
+import 'package:flutter_apk_1/repositories/card_detail_repository.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -8,6 +11,20 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPage extends State<CardPage> {
+  CardDetail? cardDetail;
+  CardDetailRepository cardDetailRepository = CardDetailRepository();
+
+  @override
+  void initState() {
+    super.initState();
+    carregarDados();
+  }
+
+  void carregarDados() async {
+    cardDetail = await cardDetailRepository.get();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,46 +32,67 @@ class _CardPage extends State<CardPage> {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: Card(
-            elevation: 8,
-            shadowColor: Colors.grey,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Image.network(
-                        "https://c5gwmsmjx1.execute-api.us-east-1.amazonaws.com/prod/dados_processo_seletivo/logo_empresa/119818/LOGO-DIO-COLOR.png_name_20221031-2831-ekn5hh.png",
-                        height: 20,
+          child: cardDetail == null
+              ? const LinearProgressIndicator()
+              : InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CardDetailPage(
+                                  cardDetail: cardDetail!,
+                                )));
+                  },
+                  child: Hero(
+                    tag: cardDetail!.id,
+                    child: Card(
+                      elevation: 8,
+                      shadowColor: Colors.grey,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Image.network(
+                                  cardDetail!.url,
+                                  height: 20,
+                                ),
+                                Text(
+                                  cardDetail!.title,
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              cardDetail!.text,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.normal),
+                              textAlign: TextAlign.justify,
+                            ),
+                            Container(
+                                width: double.infinity,
+                                alignment: Alignment.bottomRight,
+                                child: TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      "Ler Mais",
+                                      style: TextStyle(
+                                          decoration: TextDecoration.underline),
+                                    )))
+                          ],
+                        ),
                       ),
-                      const Text(
-                        "Meu Card",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
-                      ),
-                    ],
+                    ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    "O cuidado em identificar pontos críticos na necessidade de renovação processual impulsiona o reposicionamento institucional das posturas dos órgãos dirigentes com relação às suas atribuições. Ainda assim, existem dúvidas a respeito de como a execução dos pontos do programa impõe uma reinterpretação dos vínculos sociais dos índices pretendidos. À luz das boas práticas institucionais, o aumento do diálogo entre os diferentes setores produtivos causa impacto indireto na reavaliação de alternativas às soluções ortodoxas. Sob essa perspectiva, a percepção das dificuldades ressalta a relevância da participação ativa das condições financeiras e administrativas exigidas",
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                    textAlign: TextAlign.justify,
-                  ),
-                  Container(
-                      width: double.infinity,
-                      alignment: Alignment.bottomRight,
-                      child:
-                          TextButton(onPressed: () {}, child: const Text("Ler Mais",
-                          style: TextStyle(decoration: TextDecoration.underline),)))
-                ],
-              ),
-            ),
-          ),
+                ),
         ),
       ],
     );
---
+++ flutter_apk_1/lib/repositories/card_detail_repository.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_apk_1/model/card_detail.dart';
+
+class CardDetailRepository {
+
+  Future<CardDetail> get() async {
+    await Future.delayed(Duration(seconds: 3));
+    return CardDetail(
+      1,
+      "Meu Card",
+      "https://c5gwmsmjx1.execute-api.us-east-1.amazonaws.com/prod/dados_processo_seletivo/logo_empresa/119818/LOGO-DIO-COLOR.png_name_20221031-2831-ekn5hh.png",
+      "O cuidado em identificar pontos críticos na necessidade de renovação processual impulsiona o reposicionamento institucional das posturas dos órgãos dirigentes com relação às suas atribuições. Ainda assim, existem dúvidas a respeito de como a execução dos pontos do programa impõe uma reinterpretação dos vínculos sociais dos índices pretendidos. À luz das boas práticas institucionais, o aumento do diálogo entre os diferentes setores produtivos causa impacto indireto na reavaliação de alternativas às soluções ortodoxas. Sob essa perspectiva, a percepção das dificuldades ressalta a relevância da participação ativa das condições financeiras e administrativas exigidas");
+
+  }
+
+}
