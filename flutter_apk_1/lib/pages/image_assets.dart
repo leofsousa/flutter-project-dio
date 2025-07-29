95acb26b24ee135d06f7325054ae9b86dc549731
Leozera2k <leofsousa98@gmail.com>
2025-07-29 14:03:07 -0300

    images assets atualizado

--- flutter_apk_1/lib/pages/image_assets.dart
+++ flutter_apk_1/lib/pages/image_assets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apk_1/shared/app_images.dart';
 
 class ImageAssetsPage extends StatefulWidget {
   const ImageAssetsPage({super.key});
@@ -11,7 +12,28 @@ class _ImageAssetsPageState extends State<ImageAssetsPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Image.asset("assets/images/user_m.png")],
+      children: [
+        Image.asset(
+          AppImages.userf,
+          height: 50,
+        ),
+        Image.asset(
+          AppImages.userm,
+          height: 50,
+        ),
+        Image.asset(
+          AppImages.paisagem1,
+        width: double.infinity,
+        ),
+        Image.asset(
+          AppImages.paisagem2,
+          height: 50,
+        ),
+        Image.asset(
+          AppImages.paisagem3,
+          height: 50,
+        )
+      ],
     );
   }
 }
---
+++ flutter_apk_1/lib/shared/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static String get userf => "assets/images/user_f.png";
+  static String get userm => "assets/images/user_m.png";
+  static String get paisagem1 => "assets/images/paisagem1.jpeg";
+  static String get paisagem2 => "assets/images/paisagem2.jpg";
+  static String get paisagem3 => "assets/images/paisagem3.jpg";
+}
